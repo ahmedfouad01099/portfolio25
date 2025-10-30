9595aa53d44d38e65518cac5891d125e698f9ebb
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2025-10-30 21:56:03 +0300

    add docker configrations to deploy portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Required for static export (for GitHub Pages)
+  basePath: '/portfolio25', // your repo name
+  assetPrefix: '/portfolio25/', // ensure assets load correctly
+  images: {
+    unoptimized: true, // GitHub Pages doesn’t support Next.js image optimization
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
